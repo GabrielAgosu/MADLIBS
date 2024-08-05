05ae7130092856c76e78be20c7cdf354cfd3e96e
GabrielAgosu <gabrielagosu08@gmail.com>
2024-08-05 02:04:32 +0000

    madlibs

---
+++ script.js
@@ -0,0 +1,29 @@
+
+let thing= ("haunted house")
+let action= ("escaped")
+let number= (13)
+let userName = prompt("enter your name") 
+let age= prompt("how old are you")
+let calculatednumber= (200*4)
+let iSsafe= true
+
+
+
+let story=
+("Out of nowhere a "
+ + age 
+ + " year old boy named " 
+ + userName 
+ + " walked into a " 
+ + thing 
+ + ". The house was very scary and he tried very hard to " 
+ + action 
+ + ". The boy finally escape after " 
+ + number 
+ + " hours. Then he ran straight home to his mommy and she hugged him "
+ + calculatednumber
+ + " times. In conclusion, this entire story is  "
+ + iSsafe)
+
+
+console.log(story)
